3258fa831ae8bc8c4663dabc41b7e6be4431a258
MUZAFFER KILIÇASLAN <2016010205058@ogrenci.karabuk.edu.tr>
2021-03-04 21:54:54 +0300

    3 Mart

---
+++ Metotlar/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Urun urun1 = new Urun();
+            urun1.Adi = "Elma";
+            urun1.Fiyati = 5;
+            urun1.Aciklama = "Amasya elması";
+            urun1.StokAdedi = 200;
+
+            ///Urun urun2 = new Urun("Çilek", 10, "Çileek");
+
+            Urun urun2 = new Urun();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+            urun2.StokAdedi = 3;
+
+            Urun urun3 = new Urun();
+            urun3.Adi = "Kayısı";
+            urun3.Fiyati = 40;
+            urun3.StokAdedi = 40;
+
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine("Ürün Adı:"+urun.Adi);
+                Console.WriteLine("Ürün Fiyatı:"+urun.Fiyati);
+                Console.WriteLine("Ürün Açıklaması:"+urun.Aciklama);
+                Console.WriteLine("****************************************");
+            }
+
+            Console.WriteLine("*****************************************************");
+              
+            SepetManager sepetManager = new SepetManager();
+
+            foreach (Urun urun in urunler)
+            {
+                sepetManager.Ekle(urun);
+            }
+
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        List<Urun> sepet = new List<Urun>();
+
+
+        public void Ekle(Urun urun)
+        {
+            sepet.Add(urun);            
+            Console.WriteLine(urun.Adi + " sepete eklendi.");
+        }
+
+        public void Listele()
+        {
+            Console.WriteLine();
+        }
+    }
+}
