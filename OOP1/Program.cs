47ed325270b499d8c8627d8569756408ce6eae00
MUZAFFER KILIÇASLAN <2016010205058@ogrenci.karabuk.edu.tr>
2021-03-06 19:25:20 +0300

    6 Mart

---
+++ OOP1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 300;
+            product1.UnitsInStock = 3;
+
+
+            //C#'a özel nesne oluşturma!
+            Product product2 = new Product { Id = 2, CategoryId = 3, ProductName="Sandalye", UnitPrice=100, UnitsInStock=5 };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+            productManager.Update(product2);
+            productManager.Delete(product1);
+           
+
+        }
+    }
+}
